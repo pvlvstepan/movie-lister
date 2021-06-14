9343527fa6a45243ed8cdf2450389950239c4333
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-14 21:57:51 +0900

    Setup API call custom hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import useAPIrequest from "./adapters/useAPIrequest";
+
 const App = () => {
+
+  const { response, error } = useAPIrequest('https://yts.mx/api/v2/list_movies.json');
+
+  console.log(response);
+
   return (
     <h1>Hello World</h1>
   );
---
+++ src/adapters/useAPIrequest.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useAPIrequest = (APIconString) => {
+
+    const [response, setResponse] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const source = axios.CancelToken.source();
+
+        makeGETRequest(APIconString, source);
+
+        return () => {
+            source.cancel();
+        };
+    }, [APIconString]);
+
+    const makeGETRequest = async (con, source) => {
+
+        setTimeout(() => {
+            source.cancel('Response timed out');
+        }, 20000);
+
+        await axios
+            .get(con, { cancelToken: source.token }, { timeout: 20000 })
+            .then(res => {
+                setResponse(res.data);
+            })
+            .catch(e => {
+                setError(e);
+            });
+    };
+
+    return { response, error };
+};
+
+export default useAPIrequest;
