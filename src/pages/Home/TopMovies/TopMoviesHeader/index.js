576a5aec0ce1797993748887b1500511a4f1300d
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-15 02:17:21 +0900

    Add top movies pagination

--- src/pages/Home/TopMovies/TopMoviesHeader/index.js
+++ src/pages/Home/TopMovies/TopMoviesHeader/index.js
@@ -1,5 +1,6 @@
-import { Spacer, Flex, Heading, HStack, IconButton, Button } from '@chakra-ui/react';
+import { Spacer, Flex, Heading, HStack, IconButton, Button, Box, Text } from '@chakra-ui/react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { GoPrimitiveDot } from 'react-icons/go';
 import React from 'react';
 
 const TopMoviesHeader = ({ setPage, page, setIsLoading, type }) => {
@@ -11,9 +12,14 @@ const TopMoviesHeader = ({ setPage, page, setIsLoading, type }) => {
 
     return (
         <Flex direction='row' w='full' align='center' py={6}>
-            <Heading as='h2' fontSize='2xl'>Top {type === 'rating' ? 'Rated' : type === 'like_count' ? 'Liked' : 'Downloads'}</Heading>
-            <Spacer />
+            <Heading whiteSpace='nowrap' as='h2' fontSize='2xl'>Top {type === 'rating' ? 'Rated' : type === 'like_count' ? 'Liked' : 'Downloads'}</Heading>
+            <Box w='full' px={3}>
+                <hr />
+            </Box>
             <HStack>
+                <HStack spacing={0}>
+                    {[...Array(6)].map((e, i) => <GoPrimitiveDot opacity={i + 1 === page ? 1 : 0.3} />)}
+                </HStack>
                 <IconButton onClick={() => handlePageNav(-1)} isDisabled={page === 1 ? true : false} size='sm' icon={<FaArrowLeft />} />
                 {page < 6 && <IconButton onClick={() => handlePageNav(+1)} size='sm' icon={<FaArrowRight />} />}
                 {page === 6 && <Button size='sm' rightIcon={<FaArrowRight />}>See all</Button>}
