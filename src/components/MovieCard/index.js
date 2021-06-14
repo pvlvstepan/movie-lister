c00edac720c58675269f426d6cf183bdb50cc1d5
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-15 06:34:35 +0900

    Add conditional rendering for year & rating

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -25,9 +25,9 @@ const MovieCard = ({ img, title, year, rating, isLoading, aspect }) => {
             </Skeleton>
             <Skeleton width='min' isLoaded={!isLoading}>
                 <HStack width='full' align='center'>
-                    <Text >{year}</Text>
+                    {year > 0 && <Text >{year}</Text>}
                     <AiFillStar fontSize='20px' color='green' />
-                    <Text whiteSpace='nowrap' display='flex' dir='row' fontWeight='semibold'>{rating} / 10</Text>
+                    <Text whiteSpace='nowrap' display='flex' dir='row' fontWeight='semibold'>{rating > 0 ? rating + ' / 10' : 'No rating'}</Text>
                 </HStack>
             </Skeleton>
         </VStack>
