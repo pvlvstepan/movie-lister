46ab03c4ecad24e73482333aca016ffab565d096
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-15 04:52:28 +0900

    Add movie cards to movie secton

---
+++ src/hooks/useWidnowLocation.js
@@ -0,0 +1,29 @@
+import { useLocation } from 'react-router-dom';
+
+const useWidnowLocation = () => {
+
+    const location = useLocation();
+    let title = '';
+    let type = '';
+
+    if (location.pathname === '/movies/new') {
+        title = 'New Releases';
+        type = 'year';
+    } else if (location.pathname === '/movies/rating') {
+        title = 'Top Rated';
+        type = 'rating';
+    } else if (location.pathname === '/movies/likes') {
+        title = 'Top Likes';
+        type = 'like_count';
+    } else if (location.pathname === '/movies/downloads') {
+        title = 'Top Downloads';
+        type = 'download_count';
+    } else {
+        title = 'Recently Added';
+        type = 'date_added';
+    }
+
+    return { title, type };
+};
+
+export default useWidnowLocation;
--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import { Heading, Box, Divider, SimpleGrid, GridItem } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import useAPIrequest from '../../adapters/useAPIrequest';
+import MovieCard from '../../components/MovieCard';
+import useWidnowLocation from '../../hooks/useWidnowLocation';
 
 const Movies = () => {
+
+    const { title, type } = useWidnowLocation();
+
+    const [page, setPage] = useState(1);
+
+    const { response, error } = useAPIrequest('https://yts.mx/api/v2/list_movies.json?sort_by=' + type + '&page=' + page);
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (response && response !== null) {
+            setIsLoading(false);
+        } else {
+            setIsLoading(true);
+        }
+    }, [response]);
+
     return (
-        <h1>Movie Page</h1>
+        <Box p={6} bg='gray.700'>
+            <Heading as='h1' fontSize='3xl'>{title}</Heading>
+            <Divider my={6} />
+            <SimpleGrid w='full' columns={4} spacing={6}>
+                {response && response.data.movies.map((val, key) => {
+                    return (
+                        <GridItem key={key}>
+                            <MovieCard
+                                img={val['medium_cover_image']}
+                                title={val['title_english']}
+                                year={val['year']}
+                                isLoading={isLoading}
+                                rating={val['rating']} />
+                        </GridItem>
+                    );
+                })}
+            </SimpleGrid>
+        </Box>
     );
 };
 
