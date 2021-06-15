2003804f96d597c654c7f3ea8528d42c3e6e8777
Stepan Pavlov <pvlv.stepan@gmail.com>
2021-06-16 02:46:38 +0900

    Add search functionality

---
+++ src/components/Navbar/InputAsButton/index.js
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import { IoSearchSharp } from 'react-icons/io5';
+import { Kbd, InputGroup, InputLeftElement, InputRightElement, Input, useColorModeValue, Button } from "@chakra-ui/react";
+
+const InputAsButton = ({ onOpen }) => {
+
+    useEffect(() => {
+        const handleBodyKeyDown = (e) => {
+            if (e.keyCode === 83 && e.altKey) {
+                onOpen();
+            }
+        };
+        document.body.addEventListener('keyup', handleBodyKeyDown);
+    }, [onOpen]);
+
+    return (
+        <InputGroup maxW='600px'>
+            <InputLeftElement pointerEvents="none" children={<IoSearchSharp />} />
+            <Input
+                as={Button}
+                justifyContent="start"
+                fontWeight={400}
+                onClick={onOpen}
+                variant="filled">
+                Search Movies
+            </Input>
+            <InputRightElement mr={6} display={{ base: "none", lg: "flex" }} pointerEvents="none" children={<><Kbd>alt</Kbd> + <Kbd>s</Kbd></>} />
+        </InputGroup>
+    );
+};
+
+export default InputAsButton;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,73 @@
-import { Heading, HStack, Input } from '@chakra-ui/react';
+import { AspectRatio, Box, Button, Divider, Heading, HStack, Image, Input, InputGroup, InputLeftElement, Modal, ModalBody, ModalContent, ModalHeader, ModalOverlay, useDisclosure, VStack } from '@chakra-ui/react';
 import { ImVideoCamera } from 'react-icons/im';
-import React from 'react';
+import React, { useState } from 'react';
 import ColorModeToggle from './ColorModeToggle';
+import { Link, useHistory } from 'react-router-dom';
+import InputAsButton from './InputAsButton';
+import { IoSearchSharp } from 'react-icons/io5';
+import useAPIrequest from '../../adapters/useAPIrequest';
 
 const Navbar = () => {
+
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [SearchTerm, setSearchTerm] = useState('');
+    const history = useHistory();
+
+    const { response, error } = useAPIrequest('https://yts.mx/api/v2/list_movies.json?query_term=' + SearchTerm.toLowerCase());
+
+    console.log('https://yts.mx/api/v2/list_movies.json?query_term=' + SearchTerm.toLowerCase());
+
+    console.log(response);
     return (
-        <HStack as='header' py={6}>
-            <HStack fontSize='4xl' maxW='220px' w='full'>
+        <HStack as='header' py={6} justify='space-between'>
+            <HStack as={Link} to='/' fontSize='4xl' maxW='220px' w='full'>
                 <ImVideoCamera />
                 <Heading as='h2' fontSize='2xl'>MovieLister</Heading>
             </HStack>
-            <Input placeholder='Search Movies' variant='filled'></Input>
-            <ColorModeToggle />
+            <InputAsButton onOpen={onOpen} />
+            <HStack>
+                <Button px={9}>Your Profile</Button>
+                <ColorModeToggle />
+            </HStack>
+            <Modal isOpen={isOpen} onClose={() => {
+                onClose();
+                setSearchTerm('');
+            }}>
+                <ModalOverlay />
+                <ModalContent rounded="xl" p={3} mx={3}>
+                    <ModalHeader p={0}>
+                        <InputGroup>
+                            <InputLeftElement pointerEvents="none" children={<IoSearchSharp />} />
+                            <Input
+                                onChange={e => { setSearchTerm(e.target.value); }}
+                                type="text"
+                                placeholder="Search Movies"
+                                variant="filled"
+                            />
+                        </InputGroup>
+                    </ModalHeader>
+                    <ModalBody
+                        p={0} mt={6}
+                        display={SearchTerm === '' ? "none" : "block"}>
+                        {(response && response.data.movies) && <VStack w='full'>
+                            {response.data.movies.map((val, key) => {
+                                return (
+                                    <>
+                                        <Divider />
+                                        <HStack cursor='pointer' onClick={() => history.push("?movie_id=" + val.id)} key={key} w='full' spacing={6}>
+                                            <AspectRatio ratio={2 / 3} w='10%' objectFit="cover">
+                                                <Image alt={val.title} fit='cover' rounded='lg' src={val['small_cover_image']} />
+                                            </AspectRatio>
+                                            <Heading maxW='80%' as='h3' fontSize='xl'>{val['title_long']}</Heading>
+                                        </HStack>
+                                    </>
+                                );
+                            })}
+                        </VStack>}
+                        {(response && !response.data.movies) && <Heading as='h4' fontSize='lg'>No search results...</Heading>}
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
         </HStack>
     );
 };
